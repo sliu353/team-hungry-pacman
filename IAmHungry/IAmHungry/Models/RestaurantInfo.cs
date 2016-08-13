aae1452361bcbeb8526dd654dc0a83a4fe909532
Siyuan Liu <sliu353@aucklanduni.ac.nz>
2016-08-14 09:38:55 +1200

    Liu's First Commit

---
+++ IAmHungry/IAmHungry/Models/RestaurantInfo.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IAmHungry.Models
+{
+    public class RestaurantInfo
+    {
+        public List<string> RestaurantJson { get; set; }
+    }
+
+}
